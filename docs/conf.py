3ff7e46d5fbe1fa509d19768d7e7623d744687d2
eriktoller <erik@toller.se>
2025-09-19 11:55:29 +0200

    updated docs with logo

--- docs/conf.py
+++ docs/conf.py
@@ -49,6 +49,7 @@ master_doc = "index"
 
 html_theme = "pydata_sphinx_theme"
 html_title = "gwrefpy"
+html_logo = "_static/figures/logo.png"
 html_theme_options = {
     "navbar_align": "content",
     "icon_links": [
