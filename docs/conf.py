19a8ab0d36fd60cf9e40de2d3a5acc457303125c
eriktoller <erik@toller.se>
2025-11-18 21:45:32 +0100

    Updated documentation
    (cherry picked from commit 6685e09d316c7480869f0e63fc1096aba8fd169b)

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,11 @@ extensions = [
     "myst_nb",
 ]
 
+myst_enable_extensions = [
+    "amsmath",
+    "dollarmath",
+]
+
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
