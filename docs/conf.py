a8fda759440c2c25da85f7c711060d813df1952c
eriktoller <erik@toller.se>
2025-11-21 14:26:31 +0100

    updated version number to v0.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "gwrefpy"
 copyright = ""
 author = ""
-release = "0.3.2"
+release = "0.4.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
